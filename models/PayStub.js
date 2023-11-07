25088b583028e208c0920092efc7ddaa0005fb2f
csc530 <77406318+csc530@users.noreply.github.com>
2023-11-07 18:36:39 -0500

    feat(paystub): :label: add class documentation
    encapsulate instance variables

--- models/PayStub.js
+++ models/PayStub.js
@@ -1,41 +1,94 @@
-class PayStub{
-    constructor(amount, start, end, shift) {
+/**
+ * @class
+ * paystub
+ */
+class PayStub {
+	/**
+       * @private
+       * @type {Number}
+       * monetary amount earned
+       */
+	#amount;
+	/**
+     * @private
+     * @type {Date}
+     * the beginning date for pay period
+     */
+	#start;
+	/**
+       * @private
+       * @type {Date}
+       * @description
+       * the last date for the pay period
+       */
+	#end;
+	/**
+     * @private
+     * @type {Shift[]}
+     * @description
+     * the shift that contribute to this Paystub
+     */
+	#shifts;
 
-        this.amount = amount;
-        this.start = start;
-        this.end = end;
-        this.shift = shift;
-    }
+	/**
+       *
+       * @param {number} amount - total paid amount
+       * @param {Date} start - the beginning of the pay period
+       * @param {Date} end - the end of the pay period
+       * @param {Shift[]} shifts - contributed shifts to the paystub
+       */
+	constructor(amount, start, end, shifts) {
+		this.#amount = amount;
+		this.#start = start;
+		this.#end = end;
+		this.#shifts = shifts;
+	}
 
-    get amount() {
-        return this._amount;
-    }
+	get amount() {
+		return this.#amount;
+	}
 
-    set amount(value) {
-        this._amount = value;
-    }
+	set amount(value) {
+		if (value < 0) {
+			throw new Error("invalid amount: amount must be positive");
+		}
+		this.#amount = value;
+	}
 
-    get start() {
-        return this._start;
-    }
+	/**
+   * @description
+   * the beginning date for pay period
+   * @returns {Date}
+   * @memberof PayStub
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date}
+   */
+	get start() {
+		return this.#start;
+	}
 
-    set start(value) {
-        this._start = value;
-    }
+	set start(value) {
+		if (this.#end < this.#start) {
+			throw new Error("invalid start date: start date must be before end date");
+		}
+		this.#start = value;
+	}
 
-    get end() {
-        return this._end;
-    }
+	get end() {
+		return this.#end;
+	}
 
-    set end(value) {
-        this._end = value;
-    }
+	set end(value) {
+		if (this.#end > this.#start) {
+			throw new Error("invalid end date: end date must be after start date");
+		}
+		this.#end = value;
+	}
 
-    get shift() {
-        return this._shift;
-    }
+	get shifts() {
+		return this.#shifts;
+	}
 
-    set shift(value) {
-        this._shift = value;
-    }
+	set shifts(value) {
+		this.#shifts = value;
+	}
 }
--- readme.md
+++ readme.md
@@ -12,6 +12,8 @@
   - [Installation](#installation)
   - [Usage](#usage)
     - [Source](#source)
+  - [Development](#development)
+    - [Documentation](#documentation)
   - [Contributing](#contributing)
   - [License](#license)
 
@@ -25,13 +27,25 @@
 
 ### Source
 
+![ESLint Badge](https://img.shields.io/badge/ESLint-4B32C3?logo=eslint&logoColor=fff&style=for-the-badge)
+![JavaScript Badge](https://img.shields.io/badge/JavaScript-F7DF1E?logo=javascript&logoColor=000&style=for-the-badge)
+![Express Badge](https://img.shields.io/badge/Express-000?logo=express&logoColor=fff&style=for-the-badge)
+![Firebase Badge](https://img.shields.io/badge/Firebase-FFCA28?logo=firebase&logoColor=000&style=for-the-badge)
+![Handlebars.js Badge](https://img.shields.io/badge/Handlebars.js-000?logo=handlebarsdotjs&logoColor=fff&style=for-the-badge)
+
 clone the repository and download the required npm packages
 
 ```cmd
-git clone https://github.com/leestephens12/PayrollSystem` &&
+git clone https://github.com/leestephens12/PayrollSystem &&
 cd PayrollSystem &&
 npm i
 ```
+
+## Development
+
+### Documentation
+
+All related types and classes should be annotated using [jsdoc](https://jsdoc.app/).
 
 ## Contributing
 
