bc74c64b0b8d57fdcd1cda340b151d3e6c7ad9d7
leestephens12 <lrsteph1@lakeheadu.ca>
2023-11-09 14:36:28 -0500

    Employee Login
    Employees can login in (hardcoded email and password for now)

--- public/javascripts/Employee.js
+++ public/javascripts/Employee.js
@@ -1,3 +1,20 @@
+const {initializeApp} = require('@firebase/app');
+const {getAuth, signInWithEmailAndPassword} = require('@firebase/auth');
+const {getFirestore, doc, getDoc} = require('@firebase/firestore');
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCIPDGbcsCLfq3JBKA6m5q36DrT8ponHRo",
+    authDomain: "payrollsystem-1b6ee.firebaseapp.com",
+    projectId: "payrollsystem-1b6ee",
+    storageBucket: "payrollsystem-1b6ee.appspot.com",
+    messagingSenderId: "37884169561",
+    appId: "1:37884169561:web:0fc7a7309a335014c95048",
+    measurementId: "G-WR9YMW4QV6"
+  });
+  
+  //Create Variables for firestore functions
+  const auth = getAuth(firebaseApp);
+  const db = getFirestore(firebaseApp);
 class Employee {
     //constructor for employee
     constructor(employeeId, fname, lname, status, manager, shifts) {
@@ -11,9 +28,25 @@ class Employee {
     }
     //methods go here
     // employees can log in to the system as long as they exist in DB
-    login() {
+    static login(email, password) {
+        //Logs employee in, using firebase authentication
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in and user object is now stored in user object
+                const user = userCredential.user;
+                console.log("signed in user: " + user.uid );
 
+            })
+            .catch((error) => { //if there is an error signing in then display the error message
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log("Message:" + errorMessage + " Error Code: " + errorCode);
+            });
     }
+
+    logout() {
+    }
+
     getPay() {
 
     }
@@ -21,4 +54,4 @@ class Employee {
     generatePayStub() {
 
     }
-}
+}module.exports = Employee;
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,19 @@
-const {initializeApp} = require('@firebase/app');
-const {getAuth} = require('@firebase/auth');
-const {getFirestore} = require('@firebase/firestore');
-
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyCIPDGbcsCLfq3JBKA6m5q36DrT8ponHRo",
-  authDomain: "payrollsystem-1b6ee.firebaseapp.com",
-  projectId: "payrollsystem-1b6ee",
-  storageBucket: "payrollsystem-1b6ee.appspot.com",
-  messagingSenderId: "37884169561",
-  appId: "1:37884169561:web:0fc7a7309a335014c95048",
-  measurementId: "G-WR9YMW4QV6"
-});
-
+//Create Variables for firestore functions
 const express = require('express');
 const router = express.Router();
-const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp)
+
+//class imports
+const Employee = require('../public/javascripts/Employee');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res){
+  //static method in employee class so we can call without an instance of the object
+  Employee.login("lrsteph1@lakeheadu.ca", "admin123");
+  
   res.render('index', { title: 'Express' });
 });
 
