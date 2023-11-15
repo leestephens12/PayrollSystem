bb94774b06a61d83683a0e1b8a0d4c7567268867
csc530 <77406318+csc530@users.noreply.github.com>
2023-11-15 11:37:53 -0500

    feat(paystub): :construction: create page to view and download employee paystub
    just scaffolded out the page template and basic framework for the router

--- routes/employee/paystub.js
+++ routes/employee/paystub.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const Database = require("../../models/utility/database");
 const {Router} = express;
 const router = Router();
 
 
 
-router.get("/employee/paystub", (req, res, next) => {
-	res.render("employee/paystub", { title: "Paystub" });
+router.get("/employee/paystub", async (req, res, next) => {
+	//todo: add way to check for authentication
+	if(!req.isAuthenticated()||req.session.user == null) {
+		res.redirect("/login");
+		return;
+	}
+
+	//todo: standardize way to get current  user; store in session or store id in session and retrieve from db
+	const employee =  await Database.getEmployee(id) || req.user;
+	res.render("employee/paystub", { title: "Paystub", paystubs: employee.paystubs });
 });
 
 module.exports= router;
