1df1dd251ec539eeeaa26ddf56382786886fdd4a
leestephens12 <lrsteph1@lakeheadu.ca>
2023-11-16 12:12:42 -0500

    Login and logout functionality complete

--- models/utility/authentication.js
+++ models/utility/authentication.js
@@ -1,24 +1,39 @@
 const {initializeApp} = require('@firebase/app');
-const {getAuth, signInWithEmailAndPassword} = require('@firebase/auth');
-
+const {getAuth, signInWithEmailAndPassword, signOut} = require('@firebase/auth');
+//calls teh firebase conifguration file in the firestore folder to get configurations
 const {firebaseConfig} = require('../../firestore/firebase-configuration');
 
 class Authentication {
-    static app = initializeApp(firebaseConfig);
-    static auth = getAuth(this.app);
-
+    //Uses the client side firebase configurations to initialize the app to a private variable
+    static #app = initializeApp(firebaseConfig);
+    //initializes the firebase authentication with the app
+    static #auth = getAuth(this.#app);
+    /**
+     * 
+     * @param {string} email received from routes/login.js
+     * @param {string} password password received from routes/login.js
+     */
     static async login(email, password) {
-        signInWithEmailAndPassword(this.auth, email, password) 
+        signInWithEmailAndPassword(this.#auth, email, password) 
         .then((userCredential) => {
-                // Signed in 
+                // if all goes well then user is signed in
                 const user = userCredential.user;
                 console.log(user.uid + " is signed in");
-                // ...
             })
             .catch((error) => {
+                //if there is an error it will output the code and message
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 console.log(errorCode + " " + errorMessage);
             });
+    }
+
+    //Logs out current user 
+    static async logout() {
+        signOut(auth).then(() => {
+            console.log("user is logged out");
+          }).catch((error) => {
+            console.log("error logging out: " + error);
+          });
     }
 } module.exports = Authentication;
