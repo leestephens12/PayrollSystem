d845224339381e1c9b13dfc2e9c89beab78ae9c2
mason8766 <mldougla@lakeheadu.ca>
2023-11-18 17:06:04 -0500

    added basic way to communicate between server and client js

--- routes/webapp.js
+++ routes/webapp.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Database = require("../models/utility/database");
+const Shift = require("../models/Shift");
 //class imports
 const Employee = require('../models/Employee');
 
@@ -8,5 +10,14 @@ router.get('/', function(req, res, next) {
   res.render('webapp');
 });
 
+router.post('/', function(req, res){
+    var employeeId = req.body.EmployeeID;
+    console.log(employeeId)
+    res.redirect('/webapp?result='+employeeId);
+    // if (employeeId = "12345")
+    //     res.redirect('/webapp?result=ClockedInSuc');
+    // else
+    // res.redirect('/webapp?result=ClockedInFail');
+  });
 
 module.exports = router;
