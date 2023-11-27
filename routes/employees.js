a636eda313cdf1e1075833a1f29256bdfbc7a96a
leestephens12 <lrsteph1@lakeheadu.ca>
2023-11-27 12:55:12 -0500

    Added an employee table view

--- routes/employees.js
+++ routes/employees.js
@@ -3,10 +3,13 @@ const router = express.Router();
 //class imports
 const Employee = require("../models/Employee");
 const Manager = require("../models/Manager");
+const Database = require("../models/utility/database");
 
 
-router.get("/", function(req, res, next) {
-	res.render("employees", {layout: "managerLayout.hbs"});
+router.get("/", async function(req, res, next) {
+    const employees = await Database.getEmployees();
+    console.log(employees);
+	res.render("employees", {layout: "managerLayout.hbs", employees: employees});
 });
 
 module.exports = router;
