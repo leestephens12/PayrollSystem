74b062d976131c3391ecef5d444ce2270eeca0cf
csc530 <77406318+csc530@users.noreply.github.com>
2023-11-30 18:50:01 -0500

    style(shift): :lipstick: add some flavour to the shifts page
    add upcoming shifts and past shifts section

--- routes/shifts.js
+++ routes/shifts.js
@@ -7,10 +7,14 @@ const Employee = require("../models/Employee");
 router.get("/", (req, res, next) => {
 	const employeeObject = req.cookies["Employee"];
 	const { _employeeID,	_firstName,		_lastName,		_department,		_permissions,		_status,		_manager,_uid, _shifts} = employeeObject;
-	const shifts = [];
 	const employee = new Employee(_employeeID, _firstName, _lastName, _department, _permissions, _status, _manager,_shifts, _uid);
+	const shifts = [];
+
+	const pastShifts = employee.shifts.filter(shift => shift.endDate < new Date());
 
 	const upcomingShifts = employee.shifts.filter(shift => shift.scheduledStart > new Date());
+
+	//create events for each upcoming shift for the (full)calendar
 	const events=[];
 	for (const shift of upcomingShifts) {
 		/** @type {import('@fullcalendar/core').EventSourceInput} */
@@ -37,7 +41,7 @@ router.get("/", (req, res, next) => {
 		},
 		events,
 	};
-	res.render("shifts", { title: "Shifts", calendarOptions,upcomingShifts });
+	res.render("shifts", { title: "Shifts", calendarOptions,upcomingShifts,shifts,pastShifts });
 });
 
 module.exports = router;
