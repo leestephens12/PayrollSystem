e6fa2f648dc3b6733d13d68f3ace9c2895f006b5
csc530 <77406318+csc530@users.noreply.github.com>
2023-11-30 23:09:44 -0500

    fix(shift): :bug: aloow for unset end and scheduled datetimes
    allow for null values to be set as the "unset" value for end dates and scheduled dates for a shift. And I updated the firestore converter to save as a list of strings as Mason implemented

--- models/Shift.js
+++ models/Shift.js
@@ -12,17 +12,14 @@ class Shift {
 
 	//#region type definitions
 	/**
-	 * @typedef {Object} ShiftDbModel
-	 * @property {Date} startDate - the beginning of the shift
-	 * @property {Date} endDate - the end of the shift
-	 */
-
-	/**
 	 * @typedef {"waiting" | "approved" | "denied" | "requestedOff" | "canceled" | "completed"} ShiftStatus
 	 * @description
 	 * status of shift
 	 *
 	 */
+	/** @type {ShiftStatus[]}
+	 * @readonly	*/
+	static ShiftStatus = ["waiting", "approved", "denied", "requestedOff", "canceled", "completed"];
 	//#endregion
 
 	/**
@@ -56,6 +53,7 @@ class Shift {
 	#scheduledEnd;
 
 	/**
+	 * todo: remove useless attribute and move up appropriate methods
 	 * @type {Employee}
 	 * @description
 	 * employee associated with the shift
@@ -75,53 +73,72 @@ class Shift {
 	static firebaseConverter = {
 		/**
 		 * @param{Shift[]} shifts
-		 * @return {ShiftDbModel[]}
+		 * @return {string[]} string representation of shifts (start date/scheduled start date/end date/scheduled end date)
 		 */
 		toFirestore: (shifts) => {
-			/**
-			 * Creates a new Shift Class object based on the given shift from firestore.
-			 *
-			 * @param {Shift} shift - The shift object to be converted.
-			 * @return {ShiftDbModel} - The new ShiftClass object.
-			 */
-			function toShiftClass(shift) {
-				return {
-					startDate: shift.startDate,
-					endDate: shift.endDate
-				};
-			}
-			return shifts.map(shift => toShiftClass(shift));
+			return shifts.map(shift => shift.toFirestoreString());
 		},
 		/**
 		 * converts shift objects to Shift class from firestore
-		 * @param {ShiftDbModel[]} shifts shifts from firestore
+		 * @param {string[]} shifts shifts from firestore
 		 * @returns {Shift[]} shifts
 		 */
 		fromFirestore: (shifts) => {
-			/**
-			 * Creates a new Shift object based on the given shift from firestore.
-			 *
-			 * @param {ShiftDbModel} shift - The shift object to be converted.
-			 * @return {Shift} - The new Shift object.
-			 */
-			function toShift(shift) {
-				if(isNullOrUndefined(shift))
-					return [];
-				if(!(shift.startDate instanceof Date))
-					throw new Error("invalid date: start date must be a Date object");
-				if(!( shift.endDate instanceof Date))
-					throw new Error("invalid date: end date must be a Date object");
-				return new Shift(shift.startDate, shift.endDate);
-			}
-			return shifts.map(shift => toShift(shift));
+			const shiftList = [];
+			if(shifts.length >0)
+				shifts.forEach(element => {
+					const stringArray = element.split("/");
+					const startDateString = stringArray[0];
+					const schStartString = stringArray[1];
+					const endDateString = stringArray[2];
+					const schEndString = stringArray[3];
+					const shift = Shift.fromFirestoreDataString(startDateString, endDateString, schStartString, schEndString);
+					shiftList.push(shift);
+				});
+			return shiftList;
 		},
 	};
 
+	/**
+	 * Creates a new Shift object from the provided date strings.
+	 *
+	 * @param {string} startDateString - The start date string of the shift.
+	 * @param {string} endDateString - The end date string of the shift.
+	 * @param {string} scheduledStartString - The scheduled start date string of the shift.
+	 * @param {string} scheduledEndString - The scheduled end date string of the shift.
+	 * @return {Shift} - The newly created Shift object.
+	 */
+	static fromFirestoreDataString(startDateString, endDateString,scheduledStartString, scheduledEndString) {
+		const startDate = startDateString === "null" ? null : new Date(startDateString);
+		const endDate = endDateString === "null" ? null : new Date(endDateString);
+		const scheduledStart = scheduledStartString === "null" ? null : new Date(scheduledStartString);
+		const scheduledEnd = scheduledEndString === "null" ? null : new Date(scheduledEndString);
+		return new Shift(startDate, endDate, scheduledStart, scheduledEnd);
+	}
+
+	/**
+	 * Generates a Firestore string representation of the date and time values of the current shift.
+	 *
+	 * @return {string} A string representing the date and time values in the format "startDate/scheduledStart/endDate/scheduledEnd".
+	 */
+	toFirestoreString() {
+		return `${this.startDate}/${this.scheduledStart}/${this.endDate}/${this.scheduledEnd}`;
+	}
+
+	/**
+	 * @description
+	 * creates a new shift
+	 * @param {Date} startDateTime the actual start time (and date) of the shift
+	 * @param {Date} endDateTime the actual end time (and date) of the shift
+	 * @param {Date} scheduledStart the scheduled start time (and date) of the shift
+	 * @param {Date} scheduledEnd the scheduled end time (and date) of the shift
+	 * @throws {Error} if any
+	 */
 	constructor(startDateTime, endDateTime, scheduledStart, scheduledEnd) {
-		this.#startDate = startDateTime;
-		this.#endDate = endDateTime;
-		this.#scheduledStart = scheduledStart;
-		this.#scheduledEnd = scheduledEnd;
+		this.startDate = startDateTime;
+		this.endDate = endDateTime ?? null;
+		this.scheduledStart = scheduledStart ?? null;
+		this.scheduledEnd = scheduledEnd ?? null;
 	}
 
 	get startDate() {
@@ -135,9 +152,9 @@ class Shift {
 	 */
 	set startDate(value) {
 		if(!(value instanceof Date))
-			throw new Error("invalid date: date must be a Date object");
+			throw new Error("invalid date: start date must be a Date object");
 		if(isNullOrUndefined(value))
-			throw new Error("invalid date: date cannot be null or undefined");
+			throw new Error("invalid date: start date cannot be null or undefined");
 		if(value > this.#endDate)
 			throw new Error("invalid date: start date must be before end date");
 		this.#startDate = value;
@@ -148,11 +165,17 @@ class Shift {
 	}
 
 	set endDate(value) {
-		if(!(value instanceof Date) && value != 'undefined')
-			throw new Error("invalid date: date must be a Date object");
-		// if(isNullOrUndefined(value))
-		// 	throw new Error("invalid date: date cannot be null or undefined");
-		if(value < this.#startDate)
+		if(value === null)
+		{
+			this.#endDate = null;
+			return;
+		}
+
+		if(!(value instanceof Date))
+			throw new Error("invalid date: end date must be a Date object or null");
+		if(value === undefined)
+			throw new Error("invalid date:	end date cannot be undefined");
+		if(value !== null && value < this.#startDate)
 			throw new Error("invalid date: end date must be after start date");
 		this.#endDate = value;
 	}
@@ -166,7 +189,7 @@ class Shift {
 			throw new Error("invalid status: status must be a string");
 		if(isNullOrUndefined(value))
 			throw new Error("invalid status: status cannot be null or undefined");
-		if(!(value == "waiting" || value == "approved" || value == "denied" || value == "requestedOff" || value == "canceled" || value == "completed"))
+		if(Shift.ShiftStatus.find(s => s.toLocaleLowerCase() === value.toLocaleLowerCase()) === undefined)
 			throw new Error("invalid status: status must be one of: waiting, approved, denied, requestedOff, canceled, completed");
 		else
 			this.#status = value;
@@ -177,12 +200,18 @@ class Shift {
 	}
 
 	set scheduledStart(value) {
-		if(!(value instanceof Date) && value != 'undefined')
-			throw new Error("invalid date: date must be a Date object");
-		// if(isNullOrUndefined(value))
-		// 	throw new Error("invalid date: date cannot be null or undefined");
+		if(value === null)
+		{
+			this.#scheduledStart = null;
+			return;
+		}
+
+		if(!(value instanceof Date))
+			throw new Error("invalid date: scheduled start date must be a Date object or null");
+		if(value === undefined)
+			throw new Error("invalid date: scheduled start date cannot be undefined");
 		if(value > this.scheduledEnd)
-			throw new Error("invalid date: start date must be before the scheduled end date");
+			throw new Error("invalid date: scheduled start date must be before the scheduled end date");
 		this.#scheduledStart = value;
 	}
 
@@ -191,12 +220,18 @@ class Shift {
 	}
 
 	set scheduledEnd(value) {
-		if(!(value instanceof Date) && value != 'undefined')
-			throw new Error("invalid date: date must be a Date object");
-		// if(isNullOrUndefined(value))
-		// 	throw new Error("invalid date: date cannot be null or undefined");
+		if(value === null)
+		{
+			this.#scheduledEnd = null;
+			return;
+		}
+
+		if(!(value instanceof Date ))
+			throw new Error("invalid date: scheduled end date must be a Date object or null");
+		if(value === undefined)
+			throw new Error("invalid date: scheduled end date cannot be null or undefined");
 		if(value < this.scheduledStart)
-			throw new Error("invalid date: end date must be after the scheduled start date");
+			throw new Error("invalid date: scheduled end date must be after the scheduled start date");
 		this.#scheduledEnd = value;
 	}
 
@@ -214,7 +249,7 @@ class Shift {
 	}
 
 	getDuration() {
-		return new Period(this.#startDate, this.#endDate);
+		return new Period(this.startDate, this.endDate);
 	}
 }
 
